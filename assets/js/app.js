ac0c2c6c3c6fd0ce2586ce2f161721125ab9c8f9
DenquinMaxence <denquin.maxence.pro@gmail.com>
2021-10-26 01:11:35 +0200

    final commit, Works as Nico wanted ;)

--- assets/js/app.js
+++ assets/js/app.js
@@ -49,44 +49,74 @@ const learners = [
 	},
 ];
 
+/* ---------- START || Allows you to define which learner should be taken into account in the selection || START ---------- */
+
+const excludeLearner = [];
 const listLearners = document.querySelector('.list-learner');
-learners.forEach((learner) => {
+
+learners.forEach((learner, key) => {
 	const button = document.createElement('button');
 	button.setAttribute('class', 'btn-leaner');
 	button.innerHTML = learner.name;
 
 	button.addEventListener('click', () => {
 		button.classList.toggle('btn-red');
+
+		// Insert or remove learner from exclude list
+		const index = excludeLearner.indexOf(key);
+		index > -1 ? excludeLearner.splice(index, 1) : excludeLearner.push(key);
 	});
 
 	listLearners.appendChild(button);
 });
+
+/* ---------- END || Allows you to define which learner should be taken into account in the selection || END ---------- */
+
+/**
+ * Allows you to select a learner taking into account the list of exclusions
+ *
+ * @param {Number} firstLearner Index of the first learner selected by the program
+ * @return {Number} Index of the learner selected by the program
+ */
+function randomKey(firstLearner) {
+	let random = Math.floor(Math.random() * learners.length);
+	while (excludeLearner.includes(random) || firstLearner === random) {
+		random = Math.floor(Math.random() * learners.length);
+	}
+
+	return random;
+}
+
+/* ---------- START || Allows you to display two selected learners taking into account the exclusion list || START ---------- */
 
 const learnerBlock = document.querySelector('.information');
 const randomBtn = document.querySelector('.random-btn');
 
 function learner() {
 	learnerBlock.innerHTML = '';
+	let firstLearner;
 	for (let i = 0; i < 2; i++) {
-		const learner = learners[Math.floor(Math.random() * learners.length)];
-		const html = `
-            <div class="apprenant">
-                <div class="img-container">
-                    <img src="${learner.img}"/>
-                </div>
+		const key = randomKey(firstLearner);
+		if (i === 0) firstLearner = key;
 
-                <div>
-                    <h4>${learner.name}</h4>
-                </div>
-            </div>
-        `;
+		const learner = learners[key];
+		const html = `
+		    <div class="apprenant">
+		        <div class="img-container">
+		            <img src="${learner.img}"/>
+		        </div>
+
+		        <div>
+		            <h4>${learner.name}</h4>
+		        </div>
+		    </div>
+		`;
 
 		learnerBlock.insertAdjacentHTML('beforeend', html);
 	}
 }
 
-//DEFAULT REVIEW DISPLAY WHEN WINDOW LOADS
-window.addEventListener('DOMContentLoaded', learner);
+/* ---------- END || Allows you to display two selected learners taking into account the exclusion list || END ---------- */
 
-//RANDOM PERSON PREVIEW
+// RANDOM LEARNERS PREVIEW
 randomBtn.addEventListener('click', learner);
